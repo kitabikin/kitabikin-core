de041d6880d8cbd6511134e583e6a0056f9aee4c
rendiriz <rendirizkir@gmail.com>
2021-11-08 20:47:52 +0700

    feat: add role read

--- src/controllers/v1/role/index.js
+++ src/controllers/v1/role/index.js
@@ -6,7 +6,7 @@ const { total } = require('./role-total');
 const { list } = require('./role-list');
 const { create } = require('./role-create');
 const { read } = require('./role-read');
-// const { update } = require('./role-update');
+const { update } = require('./role-update');
 
 const router = express.Router();
 
@@ -14,6 +14,6 @@ router.get('/total', CheckToken, total);
 router.get('/', CheckToken, list);
 router.post('/', CheckToken, create);
 router.get('/:uniq', CheckToken, read);
-// router.put('/:uniq', CheckToken, update);
+router.put('/:uniq', CheckToken, update);
 
 module.exports = router;
---
+++ src/controllers/v1/role/role-update.js
@@ -0,0 +1,54 @@
+const { Ok, ErrorHandler } = require('@core/helpers/response');
+const RoleModel = require('@core/models/sso/role.model');
+
+const moment = require('moment');
+
+const update = async (req, res) => {
+  try {
+    const qUpdate = await getUpdate(req);
+
+    Promise.all([qUpdate]).then(async (responses) => {
+      const data = responses[0];
+      return Ok(data, 'Successfully changed data.', res);
+    });
+  } catch (err) {
+    return ErrorHandler(err, res);
+  }
+};
+
+async function getUpdate(req) {
+  const pUniq = req.params.uniq;
+  const idApplication = req.body.id_application;
+  const code = req.body.code;
+  const name = req.body.name;
+  const description = req.body.description;
+  const isActive = req.body.is_active;
+  const isDelete = req.body.is_delete;
+  const modifiedId = req.decoded.id_user;
+  const modifiedAt = moment().format('YYYY-MM-DD HH:mm:ss');
+
+  const update = {
+    id_role: pUniq,
+    id_application: idApplication,
+    code,
+    name,
+    description,
+    is_active: isActive,
+    is_delete: isDelete,
+    modified_id: modifiedId,
+    modified_at: modifiedAt,
+  };
+
+  const qUpdate = await RoleModel.query()
+    .first()
+    .returning('*')
+    .withGraphFetched('application')
+    .upsertGraph(update);
+
+  return qUpdate;
+}
+
+module.exports = {
+  update,
+  getUpdate,
+};
