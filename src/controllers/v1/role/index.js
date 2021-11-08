cf30c7c3b44caf589dfddae6134b59f43af50ebd
rendiriz <rendirizkir@gmail.com>
2021-11-08 20:27:47 +0700

    feat: add role create

--- src/controllers/v1/role/index.js
+++ src/controllers/v1/role/index.js
@@ -4,7 +4,7 @@ const { CheckToken } = require('@core/helpers/middleware');
 
 const { total } = require('./role-total');
 const { list } = require('./role-list');
-// const { create } = require('./role-create');
+const { create } = require('./role-create');
 // const { read } = require('./role-read');
 // const { update } = require('./role-update');
 
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.get('/total', CheckToken, total);
 router.get('/', CheckToken, list);
-// router.post('/', CheckToken, create);
+router.post('/', CheckToken, create);
 // router.get('/:uniq', CheckToken, read);
 // router.put('/:uniq', CheckToken, update);
 
