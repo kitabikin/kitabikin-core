22fb48100614846e9da67ca1b792a7aafbaafdb5
rendiriz <rendirizkir@gmail.com>
2021-12-30 14:08:25 +0700

    feat: import invitation guest book

--- src/_core/helpers/multer.js
+++ src/_core/helpers/multer.js
@@ -3,13 +3,15 @@ const multer = require('multer');
 const { v4: uuidv4 } = require('uuid');
 
 // Multer config
-module.exports = multer({
-  storage: multer.diskStorage({
-    filename: (req, file, cb) => {
-      const ext = path.extname(file.originalname);
-      cb(null, new Date().getTime() + '_' + uuidv4() + ext);
-    },
-  }),
+const storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, new Date().getTime() + '_' + uuidv4() + ext);
+  },
+});
+
+const singleImage = multer({
+  storage: storage,
   fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname);
     if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png' && ext !== '.svg') {
@@ -19,3 +21,20 @@ module.exports = multer({
     cb(null, true);
   },
 });
+
+const singleExcel = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    if (ext !== '.xls' && ext !== '.xlsx') {
+      cb(new Error('File type is not supported'), false);
+      return;
+    }
+    cb(null, true);
+  },
+});
+
+module.exports = {
+  singleImage,
+  singleExcel,
+};
--- src/controllers/v1/invitation-guest-book/index.js
+++ src/controllers/v1/invitation-guest-book/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
 const { CheckAccess, CheckToken } = require('@core/helpers/middleware');
+const { singleExcel } = require('@core/helpers/multer');
 
 const { total } = require('./invitation-guest-book-total');
 const { list } = require('./invitation-guest-book-list');
 const { create } = require('./invitation-guest-book-create');
 const { read } = require('./invitation-guest-book-read');
 const { update } = require('./invitation-guest-book-update');
+const { upload } = require('./invitation-guest-book-upload');
 
 const router = express.Router();
 
@@ -26,5 +28,8 @@ router.get('/:uniq', CheckToken, read);
 // Update
 router.put('/:uniq', [CheckAccess, CheckToken], update);
 router.put('/:uniq', update);
+
+// Upload
+router.post('/upload', [CheckToken, singleExcel.single('file')], upload);
 
 module.exports = router;
