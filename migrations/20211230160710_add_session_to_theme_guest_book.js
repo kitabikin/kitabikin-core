83d5cbf53e4c6156905ed071c79c713e55e85d3c
rendiriz <rendirizkir@gmail.com>
2021-12-30 23:34:24 +0700

    fix: add field

---
+++ migrations/20211230160710_add_session_to_theme_guest_book.js
@@ -0,0 +1,11 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.withSchema('invitation').table('theme_guest_book', function (t) {
+    t.integer('session').defaultTo(1);
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.withSchema('invitation').table('theme_guest_book', function (t) {
+    t.dropColumn('session');
+  });
+};
