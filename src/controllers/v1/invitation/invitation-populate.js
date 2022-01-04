17e5ab8bf5b1f8502996213bbde0686917cdb470
rendiriz <rendirizkir@gmail.com>
2022-01-04 21:18:06 +0700

    fix: sort greeting

--- src/controllers/v1/invitation/invitation-populate.js
+++ src/controllers/v1/invitation/invitation-populate.js
@@ -75,10 +75,16 @@ const getPopulate = (f, populate, access) => {
           break;
         case 'invitation_greeting':
           if (access === 'private') {
-            f.withGraphJoined('invitation_greeting');
+            f.withGraphJoined('invitation_greeting(orderByIGModifiedAt)');
           } else {
-            f.withGraphJoined('invitation_greeting(publicSelects)');
+            f.withGraphJoined('invitation_greeting(publicSelects, orderByIGModifiedAt)');
           }
+
+          f.modifiers({
+            orderByIGModifiedAt(builder) {
+              builder.where('invitation.invitation_guest_book.modified_at', 'desc');
+            },
+          });
           break;
         default:
           break;
