8ed97103326536865f194bf8895696e169f33bb3
Rendi Rizki <rendirizkir@gmail.com>
2022-07-27 10:27:29 +0700

    chore: vercel.json

--- knexfile.js
+++ knexfile.js
@@ -24,13 +24,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.PG_HOST,
-      port: process.env.PG_PORT,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-    },
+    connection: process.env.PG_URL,
     pool: {
       min: 2,
       max: 10,
